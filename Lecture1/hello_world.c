f659ad10f85cf2961458a6b8870889188485121b
Keaton Burleson <keaton.burleson@fedex.com>
2021-08-24 18:04:47 -0500

    Lecture 1

---
+++ Lecture1/hello_world.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/* Hello World 1.0 (not the standard way a C programmer would typically do it)
+void main()
+{
+	puts("Hello World!");
+}
+*/
+
+
+// Hello World 2.0
+int main(void)
+{
+	puts("Hello World!");
+}
