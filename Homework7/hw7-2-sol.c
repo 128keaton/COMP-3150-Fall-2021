04b07df9676f65ddb3e8fa9410f8b81459e28f45
Keaton Burleson <keaton.burleson@fedex.com>
2021-10-20 13:08:44 -0500

    Updates hw7

--- Homework7/hw7-2-sol.c
+++ Homework7/hw7-2-sol.c
@@ -18,31 +18,49 @@ int main() {
 	int creditHours;
         char letterGrade;
 
+	// Wait for a zero
         while(scanf("%d",  &creditHours) > 0) {
 		if (creditHours == 0)
 			break;
 
+		// Retrieve the letter grade inputted
 		scanf(" %c", &letterGrade);
 
+		// Add the calculated qualityPoints to the total
 		qualityPoints += (getGradeValue(letterGrade) * creditHours);
+
+		// Additionally, add the creditHours
 		creditHoursTotal += creditHours;
 
+		// Append the CREDIT_HOURS LETTER_GRADE line to the transcript.txt file
 		fprintf(transcriptHandle, "%d %c\r\n", creditHours, letterGrade);
 	}
 
+	// Calculate the GPA
 	float gpa = (float)qualityPoints/(float)creditHoursTotal;
+
+	// Finally, write the GPA line to the transcript.txt file
 	fprintf(transcriptHandle, "GPA: %.03f\r\n", gpa);
+
+	// Close the file handle to the transcript
+	fclose(transcriptHandle);
 }
 
+// This functions accepts a letter grade (A, B, C, D, F) and returns the point value
+// assocated with the letter grade. Any other letters return 0, like F
 int getGradeValue(char letterGrade) {
 	switch (letterGrade) {
 		case 'A':
+		case 'a':
 			return 4;
 		case 'B':
+		case 'b':
 			return 3;
 		case 'C':
+		case 'c':
 			return 2;
 		case 'D':
+		case 'd':
 			return 1;
 		default:
 			return 0;
